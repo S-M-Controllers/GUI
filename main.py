cff8c9be91328232536a936d86b3cf0e74dabdcb
Noah Markert <noma656b@msx.tu-dresden.de>
2020-10-22 16:17:29 +0200

    added String for state

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import tkinter as tk
 import time
 
+# TODO update State
+
+consoleState = "Connected"
 
 class Gui:
     def __init__(self):
         self.root = tk.Tk()
-        self.statusLabel = tk.Label(text="State: Connected")
+        self.statusLabel = tk.Label(text="State: " + consoleState)
         self.statusLabel.grid(row=0, column=0)
         self.timeLabel = tk.Label(text="")
         self.timeLabel.grid(row=0, column=1, padx=1000)
